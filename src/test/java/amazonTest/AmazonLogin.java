ec1ca8fff564dbacfd4a0af32a52bb22a1c8db8c
herain <herainp@gmail.com>
2021-02-14 20:41:55 +0800

    parallet test execute

--- src/test/java/amazonTest/AmazonLogin.java
+++ src/test/java/amazonTest/AmazonLogin.java
@@ -1,20 +1,21 @@
 package amazonTest;
 
 import amazonHelper.BrowserInit;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 import pageObject.HomePageObject;
 
-public class AmazonHome {
+public class AmazonLogin {
     WebDriver driver;
     String url ="https://www.amazon.com";
     HomePageObject hpo;
     BrowserInit ah;
+    JavascriptExecutor jse;
 
     @BeforeClass(alwaysRun = true)
     @Parameters({"browser"}) //can read multiple paramenter from TestNG file
@@ -22,23 +23,20 @@ public class AmazonHome {
         String[] browser_selection = browser.split(",");// spliting values
         ah = new BrowserInit();
         driver = ah.initializeDriver(browser_selection[0]);
-        driver.get(url);
+        jse = (JavascriptExecutor)driver;
         hpo = new HomePageObject(driver);
-
     }
 
-    @Test(groups = {"Regression1"})
-    public void verifyAmazonLaunch() throws InterruptedException {
-        String expectedTitle = "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
-        String actualTitle = driver.getTitle();
+    @Test
+    public void validateLogin() throws InterruptedException {
+        driver.get(url);
         Thread.sleep(2000);
-        Assert.assertEquals(actualTitle,expectedTitle);
-    }
-
-    @Test(groups = {"Regression","Sanity"})
-    public void verifySearch() {
-        hpo.enterText("Shoes");
-        hpo.clickit();
+        jse.executeScript("window.scrollBy(0,100);");
+        Thread.sleep(2000);
+        jse.executeScript("window.scrollBy(0,-100);");
+        hpo.movetoSignIn(driver);
+        Thread.sleep(2000);
+        hpo.click_Signin();
     }
 
     @AfterClass(alwaysRun = true)
