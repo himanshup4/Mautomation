a5b7e5cb959e3b05ad1abfe78355b66cd1179a43
htest <htest@gmail.com>
2021-02-14 16:26:29 +0530

    Test1

--- src/test/java/amazonTest/AmazonHome.java
+++ src/test/java/amazonTest/AmazonHome.java
@@ -1,7 +1,11 @@
 package amazonTest;
 
 import amazonHelper.BrowserInit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -14,12 +18,16 @@ public class AmazonHome {
     String url ="https://www.amazon.com";
     HomePageObject hpo;
     BrowserInit ah;
+    JavascriptExecutor jse;
 
     @BeforeClass(alwaysRun = true)
     public void initDriver(){
         ah = new BrowserInit();
         driver = ah.initializeDriver("chrome");
+        jse = (JavascriptExecutor)driver;
         driver.get(url);
+        hpo = new HomePageObject(driver);
+
     }
 
     @Test(groups = {"Regression"})
@@ -31,9 +39,20 @@ public class AmazonHome {
 
     @Test(groups = {"Regression","Sanity"})
     public void verifySearch() {
-        hpo = new HomePageObject(driver);
         hpo.enterText("Shoes");
         hpo.clickit();
+    }
+
+    @Test
+    public void validateLogin() throws InterruptedException {
+        driver.get(url);
+    Thread.sleep(2000);
+        jse.executeScript("window.scrollBy(0,100);");
+        Thread.sleep(2000);
+        jse.executeScript("window.scrollBy(0,-100);");
+        hpo.movetoSignIn(driver);
+        Thread.sleep(2000);
+        hpo.click_Signin();
     }
 
     @AfterClass(alwaysRun = true)
--- src/test/java/pageObject/HomePageObject.java
+++ src/test/java/pageObject/HomePageObject.java
@@ -1,7 +1,9 @@
 package pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -19,6 +21,11 @@ public class HomePageObject {
     @FindBy(xpath = "//div[@class='nav-search-submit nav-sprite']/span/input")
     WebElement clickButton;
 
+    @FindBy(css = "#nav-link-accountList-nav-line-1")
+    WebElement signin;
+
+    @FindBy(css = ".nav-action-inner")
+    WebElement clicksignin;
 
     public void enterText(String textToEnter){
         entertext.sendKeys(textToEnter);
@@ -26,5 +33,14 @@ public class HomePageObject {
 
     public void clickit(){
         clickButton.click();
+    }
+
+    public void movetoSignIn(WebDriver driver){
+        Actions a = new Actions(driver); // using action class to control driver
+        a.moveToElement(signin).perform(); // using action class to hover on element
+    }
+
+    public void click_Signin(){
+        clicksignin.click();
     }
 }
